79506b4d391ea52b62dab5695ce35157e9336601
Mohamed elnemr <mohamed.ahmed.aboelfath@gmail.com>
2019-03-20 23:54:40 +0200

    add tables without foreign keys

---
+++ database/migrations/2019_03_20_214206_create_coaches_sessions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoachesSessionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coaches_sessions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+//            $table->foreign('gym_id')->references('id')->on('gyms');
+//            $table->foreign('session_id')->references('id')->on('training_session');
+//            $table->foreign('coach_id')->references('id')->on('coaches');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coaches_sessions');
+    }
+}
---
+++ database/migrations/2019_03_20_214308_create_gyms_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGymsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gyms', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 100);
+            $table->string('image', 100);
+            $table->date('created_at');
+
+//            $table->foreign('manager_id')->references('id')->on('users');
+//            $table->foreign('session_id')->references('id')->on('training_session');
+//            $table->foreign('city_id')->references('id')->on('cities');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gyms');
+    }
+}
