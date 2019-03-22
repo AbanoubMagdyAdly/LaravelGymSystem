a6279a2131a464ff2346a6dcbda9bb60751dccdd
emanszayan <you@example.com>
2019-03-22 19:00:21 +0200

    Add users Api

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -26,7 +26,7 @@ class UsersController extends Controller
         User::create($request->all());
 
         return response()->json([
-            'message' => 'Post Created Successfully'
+            'message' => 'user Created Successfully'
         ],201);
     }
 
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,18 +25,6 @@ class HomeController extends Controller
      * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index()
-<<<<<<< HEAD
-    {       
-        // Role:: create(['name'=>'admin']);
-        // Role:: create(['name'=>'city_manager']);
-        // Role:: create(['name'=>'gym_manager']);
-        // $permission = Permission::create(['name' => 'CRUD_training_sessions']);
-        // $permission = Permission::create(['name' => 'assign_coaches_to_sessions']);
-        // $permission = Permission::create(['name' => 'buy_sessions_to_users']);
-        // $permission = Permission::create(['name' => 'show_city_gyms']);
-        // $permission = Permission::create(['name' => 'CRUD_gyms']);
-        // $permission = Permission::create(['name' => 'CRUD_city_gyms_manager']);
-=======
     {
 //         Role:: create(['name'=>'admin']);
 //         Role:: create(['name'=>'city_manager']);
@@ -49,7 +37,6 @@ class HomeController extends Controller
 //         $permission = Permission::create(['name' => 'CRUD_city_gyms_manager']);
 
 
->>>>>>> 2f5a36aa50945a22a5018fef019262ff9eed8549
         return view('home');
     }
 
