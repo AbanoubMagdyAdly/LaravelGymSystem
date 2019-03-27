0367f6f7c7669b538254ca7f54d943a9a2d614fc
Abdelrahman Amin <abdelrahman.amin93@gmail.com>
2019-03-27 19:33:46 +0200

    handling inactive-user last login

--- app/Console/Commands/InactiveUsers.php
+++ app/Console/Commands/InactiveUsers.php
@@ -40,13 +40,12 @@ class InactiveUsers extends Command
      */
     public function handle()
     {
-        $traniees =  Trinee::all();
+        $traniees =  Trainee::all();
         $time_now = Carbon::now();
         foreach ($traniees as $trainee) {
-            $last_login = $trainee->last_login;
+            $last_login_from_db = $trainee->last_login;
+            $last_login = Carbon::parse($last_login_from_db);
             $days = $last_login->diffInDays($time_now);
-            // $time_difference = $time_now - $last_login;
-            // $time_in_days = $time_difference /(24*60*60);
             if ($days >30) {
                 $trainee->notify(new MailNotifier());
             }
