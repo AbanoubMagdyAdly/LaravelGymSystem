93fec8766d6acf913ec20046a36044663f26e9b6
AbanoubMagdyAdly <abanoub.magdy.adly@gmail.com>
2019-03-29 17:38:15 +0200

    Remove foreign keys user

--- database/migrations/2019_03_21_134918_add_foreign_keys.php
+++ database/migrations/2019_03_21_134918_add_foreign_keys.php
@@ -38,10 +38,7 @@ class AddForeignKeys extends Migration
             $table->foreign('gym_id')->references('id')->on('gyms');
             $table->foreign('city_id')->references('id')->on('cities');
         });
-        Schema::table('users',function(Blueprint $table){
-            $table->foreign('gym_id')->references('id')->on('gyms');
-            $table->foreign('city_id')->references('id')->on('cities');
-        });
+
 
 
     }
