bfd32e068e6770ea9ffa40ddd7b62ce76d893f0b
AbanoubMagdyAdly <abanoub.magdy.adly@gmail.com>
2019-03-29 21:45:58 +0200

    Modify Gym creation

--- resources/views/coache/create.blade.php
+++ resources/views/coache/create.blade.php
@@ -4,7 +4,7 @@
             <!-- general form elements -->
             <div class="card card-primary">
               <div class="card-header">
-                <h3 class="card-title">Create XCoaches</h3>
+                <h3 class="card-title">Create Coaches</h3>
               </div>
               <!-- /.card-header -->
               <!-- form start -->
